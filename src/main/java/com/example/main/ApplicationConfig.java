abb86a71b24ab73ef58ca0306827bb91c742f050
Abu <abu170217@gmail.com>
2020-12-19 18:20:29 +0530

    rest api with front end html

---
+++ src/main/java/com/example/main/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package com.example.main;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("/webapi")
+public class ApplicationConfig extends Application {
+    
+}
